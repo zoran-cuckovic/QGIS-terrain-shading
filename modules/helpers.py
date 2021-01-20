c9ef45faa98c0ed0c21b3d76ff0b05d12efe24a1
Zoran Čučković <cuckovic.zoran@gmail.com>
2021-01-20 16:30:53 +0100

    Update helpers.py

--- modules/helpers.py
+++ modules/helpers.py
@@ -6,6 +6,11 @@ Created on Tue Apr 21 12:07:10 2020
 """
 import numpy as np
 
+from typing import List
+import math
+
+
+
 def view (offset_y, offset_x, shape, step=1, edge=0):
     """            
     Function returning two matching numpy views for moving window routines.
@@ -112,3 +117,70 @@ def filter3 (raster, average=True):
                     
     if average: temp_matrix /= temp_count
     return temp_matrix
+
+
+# Code from : https://github.com/fasiha/nextprod-py
+def nextpow(a: float, x: float) -> float:
+  """The smallest `a^n` not less than `x`, where `n` is a non-negative integer.
+  
+  `a` must be greater than 1, and `x` must be greater than 0.
+  # Examples
+  ```jldoctest
+  julia> nextpow(2, 7)
+  8
+  julia> nextpow(2, 9)
+  16
+  julia> nextpow(5, 20)
+  25
+  julia> nextpow(4, 16)
+  16
+  ```
+  """
+  assert x > 0 and a > 1
+  if x <= 1:
+    return 1.0
+  n = math.ceil(math.log(x, a))
+  p = a**(n - 1)
+  return p if p >= x else a**n
+
+
+# Code from : https://github.com/fasiha/nextprod-py
+def nextprod(a: List[int], x: int) -> int:
+  """Next integer greater than or equal to `x` that can be written as ``\\prod k_i^{a_i}`` for integers
+  ``a_1``, ``a_2``, etc.
+  # Examples
+  ```jldoctest
+  julia> nextprod([2, 3], 105)
+  108
+  julia> 2^2 * 3^3
+  108
+  ```
+  """
+  k = len(a)
+  v = [1] * k  # current value of each counter
+  mx = [nextpow(ai, x) for ai in a]  # maximum value of each counter
+  v[0] = mx[0]  # start at first case that is >= x
+  p = mx[0]  # initial value of product in this case
+  best = p
+  icarry = 1
+
+  while v[-1] < mx[-1]:
+    if p >= x:
+      best = p if p < best else best  # keep the best found yet
+      carrytest = True
+      while carrytest:
+        p = p // v[icarry - 1]
+        v[icarry - 1] = 1
+        icarry += 1
+        p *= a[icarry - 1]
+        v[icarry - 1] *= a[icarry - 1]
+        carrytest = v[icarry - 1] > mx[icarry - 1] and icarry < k
+      if p < x:
+        icarry = 1
+    else:
+      while p < x:
+        p *= a[0]
+        v[0] *= a[0]
+  return int(mx[-1] if mx[-1] < best else best)
+
+
